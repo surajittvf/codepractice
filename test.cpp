8835282987fad7b490c8eb7332eeb2477c3480c2
Surajit Dutta <surajit@Surajits-MBP.fritz.box>
2022-01-10 18:52:27 +0100

    linked List implemented

--- test.cpp
+++ test.cpp
@@ -1,9 +1,79 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node (int f_data) : 
+        data(f_data), 
+        next(nullptr)
+    {}
+};
+
+template <typename node_t>
+class List
+{
+private:
+    node_t* head;
+public:
+    List(): 
+        head(nullptr)
+    {}
+   
+    inline void push_back(int data)
+    {
+        node_t* tmp = new node_t(data);
+        if(!head)
+        {
+            head = tmp;
+            return;
+        }
+        node_t* curr = head;
+        while (curr->next)
+        {
+            curr = curr->next;
+        }
+        curr->next = tmp;
+    }
+
+    inline void push_front(int data)
+    {
+        node_t* tmp = new node_t(data);
+        tmp->next = head;
+        head = tmp;
+    }
+
+    friend ostream& operator<<(ostream& os, const List<node_t>& list)
+    {
+        node_t* curr = list.head;
+        std::string conn = "->";
+        while(curr)
+        {
+            if(!curr->next)
+            {
+                conn = "\n";
+            }
+            os << curr->data << conn;
+            curr = curr->next;
+        }
+        return os;
+    }
+};
+
+typedef List<Node> linkedList;
+
+
 
 int main()
 {
-    std::string a = "hello world.............";
-    std::cout << a << std::endl;
+    linkedList ll;
+    for(int i=1; i<=10; ++i)
+    {
+       ll.push_back(i);
+    }
+    std::cout << ll; 
     return 0;
 }
